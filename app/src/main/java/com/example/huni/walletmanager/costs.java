a56ea50e9cf079c254a608e0845483db2a300ad3
huszarhunor <h.huni56@freemail.hu>
2018-12-19 23:20:54 +0200

    real time database added

--- app/src/main/java/com/example/huni/walletmanager/costs.java
+++ app/src/main/java/com/example/huni/walletmanager/costs.java
@@ -1,15 +1,27 @@
 package com.example.huni.walletmanager;
 
-public class costs {
+public class Costs {
+    public String salary;
     public String general;
     public String housing;
+    public String finance;
+    public String transport;
+    public String drinks;
+    public String food;
+    public String entertainment;
 
-    public costs(){
+    public Costs(){
 
     }
 
-    public costs(String general, String housing) {
+    public Costs(String salary, String general, String housing, String finance, String transport, String drinks, String food, String entertaiment) {
+        this.salary = salary;
         this.general = general;
         this.housing = housing;
+        this.finance = finance;
+        this.transport = transport;
+        this.drinks = drinks;
+        this.food = food;
+        this.entertainment = entertaiment;
     }
 }
